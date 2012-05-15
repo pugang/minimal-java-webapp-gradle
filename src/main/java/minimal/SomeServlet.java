77b626c459326380d7657767fa781851c7018106
Richard C Isaacson <richard.c.isaacson@gmail.com>
2012-05-14 22:38:03 -0500

    Working now.

---
+++ src/main/java/minimal/SomeServlet.java
@@ -0,0 +1,53 @@
+/*
+ */
+package minimal;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author risaacson
+ */
+public class SomeServlet extends HttpServlet {
+
+  protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    try {
+      out.println("<html>");
+      out.println("<head>");
+      out.println("<title>This is SomeServlet!</title>");  
+      out.println("</head>");
+      out.println("<body>");
+      out.println("Some");
+      out.println("</br />");
+      out.println("Servlet");
+      out.println("</body>");
+      out.println("</html>");
+    } finally {            
+      out.close();
+    }
+  }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
